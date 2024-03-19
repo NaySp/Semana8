80e8298c9d5b64c772a16acb8912c0babfed55c8
Samuel Alvarez Alban <samuelalvarez2221@gmail.com>
2024-03-19 09:57:13 -0500

    Add chatSala

---
+++ sesion15/chatSala/Server/Chatters.java
@@ -0,0 +1,56 @@
+import java.util.Set;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Chatters {
+    // tendra una lista de personas que seran nuestros clientes
+    // cada persona tiene un nombre y un canal para enviarle mensajes
+    private Set<Person> clientes = new HashSet<>();
+
+    public Chatters() {
+    }
+
+    // metodo para verificar si un usuario existe, retorna true si existe
+    public boolean userExist(String username) {
+        for (Person person : clientes) {
+            if (person.getName().equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // metodo para agregar un usuario nuevo
+    public void addUser(Person person) {
+        boolean exist = userExist(person.getName());
+        if (exist == true) {
+            System.out.println("persona ya existe");
+        } else {
+            clientes.add(person);
+            System.out.println("persona agragada");
+        }
+
+    }
+
+    // metodo para eliminar un usuario
+    public void eliminatePerson(String username) {
+        for (Person person : clientes) {
+            if (person.getName().equals(username)) {
+                clientes.remove(person);
+                System.out.println("persona eliminada");
+            }
+        }
+        System.out.println("persona no existe");
+    }
+
+    // metodo para enviar un mensaje a todos los usuarios
+    public void sendMessageToall(String message) {
+        for (Person person : clientes) {
+            person.getOut().write(message);
+        }
+    }
+
+}
---
+++ sesion15/chatSala/Server/Server.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+
+        int PORT = 6789;
+        Chatters clientes = new Chatters(); // lista de clientes
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Servidor iniciado. Esperando clientes...");
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Nuevo cliente conectado: " + clientSocket);
+
+                // crea el objeto para gestionar al cliente y le envia la informacion necesaria
+                ClientHandler clientHandler = new ClientHandler(clientSocket, clientes);
+
+                // inicia el hilo para ese cliente
+                Thread clientThread = new Thread(clientHandler);
+                clientThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
